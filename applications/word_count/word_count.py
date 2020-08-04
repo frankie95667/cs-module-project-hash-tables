59a0fde6a86b986531d0e3a9d9f713a067c629d4
Anthony Kawa <anthonyk2020@gmail.com>
2020-08-03 20:54:32 -0700

    completed word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,18 @@
 def word_count(s):
-    # Your code here
+    chars_to_remove = set(['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&'])
+    count = {}
+    s = ' '.join(s.split())
+    s = ''.join([c for c in s if c not in chars_to_remove]).lower()
+    if s == '':
+        return {}
+    words = s.split(" ")
+    for word in words:
+        if word in count:
+            count[word] += 1
+        else:
+            count[word] = 1
+    return count
+
 
 
 
