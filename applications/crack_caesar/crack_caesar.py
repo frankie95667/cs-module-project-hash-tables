3cc1b113cfc4bd7d61c62dfd41c320541c2443f5
Anthony Kawa <anthonyk2020@gmail.com>
2020-08-06 15:01:15 -0700

    completed crack_caesar and updated readme

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,45 @@
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
 
-# Your code here
+key = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+
+# open and read contents of ciphertext.txt
+with open("ciphertext.txt") as f:
+    text = f.read()
+
+# use hash table to keep track character count
+
+# loop through characters
+# for sentence in text:
+count_chars = {}
+for char in text:
+## if key is found
+    if char in count_chars:
+##   add 1 to value of key
+        count_chars[char] += 1
+## else
+    elif char.isalpha() and char not in count_chars:
+##   assign 1 to key
+        count_chars[char] = 1
+
+# sort counted chars by frequency. most frequent being at the top, and least frequent being at the bottom
+ordered_count = [char[0] for char in sorted(count_chars.items(), key=lambda x: x[1], reverse=True)]
+
+# convert text string into an array of chars
+text_chars = list(text)
+# pre-allocate same number of slots for the decoded string as the number of characters from the original text
+decoded_str = [None] * len(text_chars)
+
+for i in range(len(ordered_count) - 1):
+    for j in range(len(text_chars)):
+        if text_chars[j] == ordered_count[i]:
+            decoded_str[j] = key[i]
+        elif decoded_str[j] is None:
+            decoded_str[j] = text_chars[j]
+
+print("".join(decoded_str))
+
+    
+
 
--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,15 +1,17 @@
 from hashtable import HashTable
-cache = HashTable(8)
+# cache = HashTable(8)
+cache = {}
 
 def expensive_seq(x, y, z):
     key = str(x) + str(y) + str(z)
     if x <= 0:
         return y + z
-    elif cache.get(key):
-        return cache.get(key)
+    # elif cache.get(key):
+    elif key in cache:
+        return cache[key]
     else:
-        cache.put(key, expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3))
-        return cache.get(key)
+        cache[key] = expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+        return cache[key]
 
 
 
